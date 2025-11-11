1148aa561b956bcf7b7d2edd8bf6ea7458fe455c
amir <jwwwnamyr6@gmail.com>
2025-11-10 23:18:22 -0800

    body

--- my-app/app/page.js
+++ my-app/app/page.js
@@ -11,14 +11,14 @@ import Header from "@/Components/Header";
 import Footer from "@/Components/Footer";
 
 const galleryImages = [
-  { src: "/images/Amosingle.jpg", alt: "تصویر ۱" },
-  { src: "/images/Couplepic2.jpg", alt: "تصویر ۲" },
-  { src: "/images/Grouppic.jpg", alt: "تصویر ۳" },
-  { src: "/images/Amosingle2.jpg", alt: "تصویر ۴" },
-  { src: "/images/Grouppic5.jpg", alt: "تصویر ۵" },
-  { src: "/images/Grouppic2.jpg", alt: "تصویر ۶" },
-  { src: "/images/Khale2.jpg", alt: "تصویر ۷" },
-  { src: "/images/Khale3.jpg", alt: "تصویر ۸" },
+  { src: "/images/Amosingle.WEBP", alt: "تصویر ۱" },
+  { src: "/images/Couplepic2.WEBP", alt: "تصویر ۲" },
+  { src: "/images/Grouppic.WEBP", alt: "تصویر ۳" },
+  { src: "/images/Amosingle2.WEBP", alt: "تصویر ۴" },
+  { src: "/images/Grouppic5.WEBP", alt: "تصویر ۵" },
+  { src: "/images/Grouppic2.WEBP", alt: "تصویر ۶" },
+  { src: "/images/Khale2.WEBP", alt: "تصویر ۷" },
+  { src: "/images/Khale3.WEBP", alt: "تصویر ۸" },
 ];
 
 export default function Home() {
